5269472ebb2328c4d65a39dcd86769ed0c37da4e
jailsonIFRN1973 <jailson.a@escolar.ifrn.edu.br>
2024-08-27 00:47:41 +0000

    3º atualização

--- controllers/diario.py
+++ controllers/diario.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, flash
+from flask import render_template, request, redirect, flash, url_for
 from database import db
 from models.diario import Diario
 from flask import Blueprint
@@ -9,3 +9,21 @@ bp_diario = Blueprint("diario", __name__, template_folder='templates')
 def recovery():
     dados = Diario.query.all()
     return render_template('diario_recovery.html', dados=dados)
+
+@bp_diario.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method=="GET":
+	    return render_template('diario_create.html')
+
+    if request.method=="POST":
+        titulo = request.form['titulo']
+        disciplina = request.form['disciplina']
+        d = Diario(titulo,disciplina)
+        db.session.add(d)
+        db.session.commit()
+        return redirect(url_for('diario.recovery'))
+
+    
+
+
+
