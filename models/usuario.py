0b529a87cacae70f703465e6300b3f031dc05b43
jailsonIFRN1973 <jailson.a@escolar.ifrn.edu.br>
2024-09-10 00:32:03 +0000

    2_atualizando

--- models/usuario.py
+++ models/usuario.py
@@ -1,7 +1,9 @@
 # É necessário importar a variável DB
-from database import db
+from utils import db
+from flask_login import UserMixin
 
 class Usuario(db.Model):
+class Usuario(db.Model, UserMixin):
   __tablename__= "usuario"
   id = db.Column(db.Integer, primary_key = True)
   nome = db.Column(db.String(100))
